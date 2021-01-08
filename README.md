d207dbbb98873b3968cf0502f748834b43b854e7
Max Yu <maxwellyu@college.harvard.edu>
2021-01-08 11:40:57 -0500

    old website

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# toppings-web
+# toppings-landing
+frontend and backend for a web portal + fully responsive landing page
---
+++ index.js
@@ -0,0 +1,36 @@
+//changes color of navbar items according to where you've scrolled
+window.onscroll = function() {scrollFunction()};
+var pink = document.getElementById("contact");
+var blue = document.getElementById("covid");
+var orange = document.getElementById("signup");
+
+  function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("navbar").style.height = "9%";
+      document.getElementById("navbar").style.borderRadius = "0px";
+      document.getElementById("navlinks").style.fontSize = "15px";
+  }
+  else 
+  { 
+      document.getElementById("navbar").style.height = "12%";
+      document.getElementById("navbar").style.borderRadius = "0px 0px 20px 20px";
+      document.getElementById("navlinks").style.fontSize = "18px";
+      
+  }
+  
+    if (window.scrollY > pink.offsetTop - 1) {
+        document.getElementById("navbar").style.filter = "hue-rotate(-28deg)";
+    }
+    else if (window.scrollY > orange.offsetTop - 1) {
+        document.getElementById("navbar").style.filter = "hue-rotate(0)";
+    }
+    else if (window.scrollY > blue.offsetTop - 1) {
+        document.getElementById("navbar").style.filter = "hue-rotate(200deg)";
+    }
+    else
+    {
+        document.getElementById("navbar").style.filter = "hue-rotate(0)";
+    }
+   }
+
+
