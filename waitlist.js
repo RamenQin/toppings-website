580e289c116ac829454bccf381ba6bcdefb537e1
Max Yu <maxwellyu@college.harvard.edu>
2021-01-19 18:39:39 -0500

    new rewards pages

--- waitlist.js
+++ waitlist.js
@@ -1,5 +1,5 @@
 function post(api_url, data, success_callback, fail_callback) {
-    localStorage.setItem('test','test')
+    
     $.ajax({
         method: 'POST',
         url: api_url,
@@ -20,7 +20,7 @@ function save_code(){
         localStorage.setItem('code',$('#referral_code').val())
         console.log($('#referral_code').val())
         console.log('test')
-        window.location.href = 'secret.html'
+        window.location.href = 'theTop.html'
         return false;
     }
     else {
@@ -33,7 +33,7 @@ function save_code2(){
         localStorage.setItem('code',$('#referral_code').val())
         console.log($('#referral_code').val())
         console.log('test')
-        window.location.href = 'mobileSecret.html'
+        window.location.href = 'theTop.html'
         return false;
     }
     else {
@@ -88,7 +88,7 @@ function submit_email_to_waitlist() {
     //alert(new_signup)
     const success_callback = function (response) {
         // fetching responses
-        console.log(response)
+        localStorage.setItem('data', JSON.stringify(response))
         waiter_email = response['registered_email']
         waiter_priority = response['current_priority']
         total_waiters_currently = response['total_waiters_currently']
@@ -110,12 +110,12 @@ function submit_email_to_waitlist() {
         // $('#demo_referral').show()
        
         // appending HTML information
-        var mylink = referral_link.substring(36)
+        var mylink = referral_link.substring(35)
         $('#current_waiter_spot').html("You are waiter " + waiter_priority)
         $('#all_waiter_spots').html(" out of " + total_waiters_currently)
         $('#total_referral').html("Referrals: " +total_referrals)
         $('#referral_link_url').html("Your Referral Code: " +mylink)
-        localStorage.setItem('test', mylink)
+        window.location.replace('./results.html')
         //$('#info_ref_link').html('Click on the button to copy your referral link (also sent to your email).')
         //window.location.href = 'secret.html'
     };
@@ -137,7 +137,72 @@ function submit_email_to_waitlist() {
             api_key: 'GPSWYI',
             referral_link: current_url
         }, success_callback, fail_callback);
-    localStorage.setItem('post','post')
+    
+};
+
+function sign_in(){
+
+    var new_signup = document.getElementById('user_email').value;
+const success_callback = function (response) {
+    // fetching responses
+   
+    waiter_email = response['registered_email']
+    waiter_priority = response['current_priority']
+    total_waiters_currently = response['total_waiters_currently']
+    referral_link = response['referral_link']
+    total_referrals = response['total_referrals']
+    user_data = `<div class="progress">
+    <div class="progress-bar progress-bar-striped active" role="progressbar"
+    aria-valuenow="${total_referrals}0" aria-valuemin="0" aria-valuemax="100" style="width:${total_referrals}0%">
+      40%
+    </div>
+  </div>`
+    console.log(waiter_email + waiter_priority + total_referrals + total_waiters_currently + referral_link)
+
+    $(".modal-content").html(user_data)
+    $("#userData").attr("style", "background-color: #007EFF;width:100%;border-radius: 5px;display: visible")
+    //$(".modal-content").css("width","200%")
+    // hiding parts of HTML
+    // $('#waitlist_email').hide()
+    // $('#demo_submit_button').hide()
+    // $('#email_address_text').hide()
+
+    // showing parts of HTML
+    // $('#current_text').show()
+    // $('#current_waiter_spot').show()
+    // $('#out_of').show()
+    // $('#all_waiter_spots').show()
+    // $('#demo_referral').show()
+   
+    // appending HTML information
+    // var mylink = referral_link.substring(36)
+    // $('#current_waiter_spot').html("You are waiter " + waiter_priority)
+    // $('#all_waiter_spots').html(" out of " + total_waiters_currently)
+    // $('#total_referral').html("Referrals: " +total_referrals)
+    // $('#referral_link_url').html("Your Referral Code: " +mylink)
+    // localStorage.setItem('test', mylink)
+    //$('#info_ref_link').html('Click on the button to copy your referral link (also sent to your email).')
+    //window.location.href = 'secret.html'
+};
+
+const fail_callback = function (response) {
+
+    // perform actions based on error codes
+    response_code = response['status']
+    if (response_code == 422) {
+        $('#error_message').html("Invalid value to sign up with.");
+    } else if (response_code == 400) {
+        $('#error_message').html("Error!");
+    }
+};
+
+post('https://www.getwaitlist.com/waitlist',
+    {
+        email: new_signup,
+        api_key: 'GPSWYI',
+        referral_link: 'https://www.toppingsapp.com'
+    }, success_callback, fail_callback);
+localStorage.setItem('post','post')
 };
 // function copy_link() {
 //     var text1 = document.getElementById("referral_link_url").textContent;
